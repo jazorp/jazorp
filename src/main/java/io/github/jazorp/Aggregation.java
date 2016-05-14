2e4b5a22ca2c82a27e90cd11ea3bde574c3a81e8
Kostas Georgiadis <kongeor@gmail.com>
2016-05-14 22:41:42 +0300

    Set spaces instead of tabs

--- src/main/java/io/github/jazorp/Aggregation.java
+++ src/main/java/io/github/jazorp/Aggregation.java
@@ -5,36 +5,36 @@ import java.util.stream.Collectors;
 
 public class Aggregation {
 
-	private Set<ValidationThunk> validations;
-	private Map<String, Aggregation> nested = new TreeMap<>();
+    private Set<ValidationThunk> validations;
+    private Map<String, Aggregation> nested = new TreeMap<>();
 
-	private Env env = Env.empty();
+    private Env env = Env.empty();
 
-	private Aggregation aggregate(ValidationThunk... thunks) {
+    private Aggregation aggregate(ValidationThunk... thunks) {
         // TODO
-		Set<ValidationThunk> hashSet = new HashSet<>(Arrays.asList(thunks));
-		validations = new TreeSet<>(hashSet);
-		return this;
-	}
+        Set<ValidationThunk> hashSet = new HashSet<>(Arrays.asList(thunks));
+        validations = new TreeSet<>(hashSet);
+        return this;
+    }
 
-	private Aggregation() {
-	}
+    private Aggregation() {
+    }
 
-	public static Aggregation of(ValidationThunk... thunks) {
-		Aggregation agg = new Aggregation();
-		agg.aggregate(thunks);
-		return agg;
-	}
+    public static Aggregation of(ValidationThunk... thunks) {
+        Aggregation agg = new Aggregation();
+        agg.aggregate(thunks);
+        return agg;
+    }
 
-	public Aggregation nested(String field, Aggregation aggregation) {
-		nested.put(field, aggregation);
-		return this;
-	}
+    public Aggregation nested(String field, Aggregation aggregation) {
+        nested.put(field, aggregation);
+        return this;
+    }
 
-	public <T> Aggregation nested(String field, Validator<T> validator, T t) {
-		nested.put(field, validator.collect(t));
-		return this;
-	}
+    public <T> Aggregation nested(String field, Validator<T> validator, T t) {
+        nested.put(field, validator.collect(t));
+        return this;
+    }
 
     public <T> Aggregation nestedList(String field, Validator<T> validator, List<T> ts) {
         for (int i=0; i<ts.size(); i++) {
@@ -48,49 +48,49 @@ public class Aggregation {
         return this;
     }
 
-	public Result validate() {
+    public Result validate() {
 
-		Result result = validateImpl(this);
+        Result result = validateImpl(this);
 
-		if (!result.wasBlocked()) {
-			for (Map.Entry<String, Aggregation> entry : nested.entrySet()) {
-				String key = entry.getKey();
-				Aggregation nestedAggr = entry.getValue();
-				Result validate = nestedAggr.validate();
-				if (!validate.isValid()) {
-					result.put(key, validate);
-				}
-			}
-		}
+        if (!result.wasBlocked()) {
+            for (Map.Entry<String, Aggregation> entry : nested.entrySet()) {
+                String key = entry.getKey();
+                Aggregation nestedAggr = entry.getValue();
+                Result validate = nestedAggr.validate();
+                if (!validate.isValid()) {
+                    result.put(key, validate);
+                }
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private Result validateImpl(Aggregation aggregation) {
+    private Result validateImpl(Aggregation aggregation) {
 
-		boolean proceed = true;
+        boolean proceed = true;
 
-		Iterator<ValidationThunk> iterator = aggregation.validations.iterator();
+        Iterator<ValidationThunk> iterator = aggregation.validations.iterator();
 
-		List<Validation> errors = new ArrayList<>();
+        List<Validation> errors = new ArrayList<>();
 
-		while(proceed && iterator.hasNext()) {
-			ValidationThunk thunk = iterator.next();
-			Validation validation = thunk.validate(env);
-			if (!validation.isValid()) {
-				errors.add(validation);
-			}
-			if (thunk.isBlocking()) {
-				proceed = false;
-			}
-		}
+        while(proceed && iterator.hasNext()) {
+            ValidationThunk thunk = iterator.next();
+            Validation validation = thunk.validate(env);
+            if (!validation.isValid()) {
+                errors.add(validation);
+            }
+            if (thunk.isBlocking()) {
+                proceed = false;
+            }
+        }
 
-		Map<String, List<Validation>> collect = errors.stream().collect(Collectors.groupingBy(v -> v.getError().getField()));
+        Map<String, List<Validation>> collect = errors.stream().collect(Collectors.groupingBy(v -> v.getError().getField()));
 
-		Map<String, Object> collect1 = new TreeMap<>(collect.entrySet().stream().collect(
-				Collectors.toMap(Map.Entry::getKey, e -> e.getValue().stream().map(Validation::getError)
-						.map(Error::getError).sorted().collect(Collectors.toList()))));
+        Map<String, Object> collect1 = new TreeMap<>(collect.entrySet().stream().collect(
+                Collectors.toMap(Map.Entry::getKey, e -> e.getValue().stream().map(Validation::getError)
+                        .map(Error::getError).sorted().collect(Collectors.toList()))));
 
-		return new Result(collect1, !proceed);
-	}
+        return new Result(collect1, !proceed);
+    }
 }
